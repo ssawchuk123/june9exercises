13a8b8bef74e967a01a3547d781e69acdac09729
Sam Sawchuk <samsawchuk@shaw.ca>
2015-06-09 14:08:13 -0400

    commit after escape dsequences

--- exercise1.rb
+++ exercise1.rb
@@ -6,10 +6,14 @@
 # name = gets.chomp
 # puts "your name is #{name}"
 
-a = 25 
-b = 70
+# a = 25 
+# b = 70
 
-puts "the value of a is #{a}"
-puts "the value of b is #{b}"
-puts "the value of a + b is #{a + b}"
+# puts "the value of a is #{a}"
+# puts "the value of b is #{b}"
+# puts "the value of a + b is #{a + b}"
 
+puts "Hello\t\tworld"
+puts "hello\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
