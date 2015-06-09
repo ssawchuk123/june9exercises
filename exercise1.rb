f6cb5f8646f0431cb80a11b8c1c1a782cabf4466
Sam Sawchuk <samsawchuk@shaw.ca>
2015-06-09 14:05:31 -0400

    After string interpolation

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,15 @@
-puts 2 
-puts 3 
-puts 2 != 3
+# puts 2 
+# puts 3 
+# puts 2 != 3
+
+# puts "Enter name"
+# name = gets.chomp
+# puts "your name is #{name}"
+
+a = 25 
+b = 70
+
+puts "the value of a is #{a}"
+puts "the value of b is #{b}"
+puts "the value of a + b is #{a + b}"
+
