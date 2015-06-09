3ede6c34a408dfbcc36b604e51b4232c54e575a2
Sam Sawchuk <samsawchuk@shaw.ca>
2015-06-09 14:31:37 -0400

    after the else section of exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,8 +1,28 @@
-puts "What is your name?"
-name = gets.chomp
+# puts "What is your name?"
+# name = gets.chomp
 
-puts "Hi #{name}, how old are you?"
+# puts "Hi #{name}, how old are you?"
 
-age = gets.chomp.to_i
+# age = gets.chomp.to_i
 
-puts "#{name}, your age is #{age}"
+# puts "#{name}, your age is #{age}"
+
+# if age >= 21
+# 	puts "You did it!"
+# else 
+# 	puts "You are not old enough!"
+# end
+
+x = 3
+y = -10
+
+def check_sign(number)
+
+	if number > 0
+		"#{number} is positive"
+	else
+		"#{number} is negative"
+	end
+end
+
+
